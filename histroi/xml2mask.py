5e9c428c2e2a6ee4bd31c8f2484e5fb47281e830
ihuszar <aideinguaf>
2019-11-27 16:18:11 +0000

    commit: version 2.0 release

--- histroi/xml2mask.py
+++ histroi/xml2mask.py
@@ -7,6 +7,7 @@
 """
 Author: Istvan N. Huszar, M.D. <istvan.huszar@dtc.ox.ac.uk>
 Date: 9-June-2019
+Last update: 27-November-2019
 
 CLI interface script to convert XML annotations to binary image masks of a
 given resolution.
@@ -18,8 +19,9 @@ given resolution.
 
 import os
 import sys
-from . import roi
 import argparse
+import numpy as np
+from histroi import roi
 
 
 # DEFINITIONS
@@ -44,19 +46,22 @@ def set_args(parser):
                          default=[1, 1], nargs="+",
                          help="Scaling factors for the x (hor.) and y (vert.) "
                               "coordinates.")
-    numargs.add_argument("--target", metavar=("<height>", "<width>"), type=int,
+    numargs.add_argument("--target", metavar=("<width>", "<height>"), type=int,
                          default=None, nargs=2,
-                         help="Target mask shape (vertical, horizontal).")
-    numargs.add_argument("--image", metavar=("<file>/\"auto\"/<x>", "<y>"),
-                         default=None, nargs="+",
-                         help="Original image shape (vertical, horizontal).")
+                         help="Target mask shape (width, height).")
+    numargs.add_argument("--image", metavar="<file>", type=str,
+                         default="auto", help="Original image.")
+    numargs.add_argument("--resolution", metavar="<level>", type=str,
+                         default="low", help="Histology resolution "
+                         "level (\"high\", \"low\" or a number)")
     numargs.add_argument("--fill", metavar="<value>", type=int, default=255,
                          help="Mask fill value for the ROI. (Default: 255)")
     numargs.add_argument("--tile", type=int, nargs=4, default=None,
-                         required=False, metavar=("<x>", "<y>", "<width>",
-                                                  "<height>"),
-                         help="Export mask for a specific (scaled) tile. "
-                              "The --image option must also be specified.")
+                         required=False,
+                         metavar=("<x>", "<y>", "<width>", "<height>"),
+                         help="Export mask for a specific tile. "
+                              "(The coordinates and dimensions of the tile "
+                              "should be given w.r.t. the output shape.")
 
     boolargs = parser.add_argument_group("Boolean modulators")
     boolargs.add_argument("--display", action="store_true", default=False,
@@ -67,6 +72,9 @@ def set_args(parser):
                           help="Do not save polygonal selection object.")
     boolargs.add_argument("--nomask", help="Do not save binary ROI mask.",
                           action="store_true", default=False)
+    boolargs.add_argument("--histo", action="store_true", default=False,
+                          help="Export the histological image or part of it "
+                               "that corresponds to the generated binary mask.")
     boolargs.add_argument("--verbose", metavar="<level>", default=40,
                           type=int, help="Verbosity level (0-40, default: 40).")
 
@@ -83,66 +91,91 @@ def main(p):
 
     options = roi.DEFAULT_OPTIONS.copy()
     options.update({
-        "original_shape": p.image,
+        "image": p.image,
+        "dimlevel": 0,
+        "dimscale_x": 1,
+        "dimscale_y": 1,
+        "original_shape": None,
         "scale": p.scale,
         "target_shape": p.target,
         "tile": None,
         "csv": not p.nocsv,
         "bin": not p.nobin,
         "mask": not p.nomask,
+        "histo": p.histo,
         "fill_value": p.fill,
         "display": p.display,
         "verbose": p.verbose,
         "outdir": p.out
     })
 
-    # Infer original image size by opening SVS file (if --image is auto or path)
-    imgarg = [arg for arg in p.image if arg] if p.image else None
-    if imgarg is None:
-        options.update({"original_shape": None})
-    elif len(imgarg) == 1:
-        imgarg = str(imgarg[0])
-        if os.path.isfile(imgarg):
-            import openslide
-            slideobj = openslide.open_slide(imgarg)
-            dimlevel = 0 if p.tile else -1
-            tshape = options["target_shape"] \
-                     or slideobj.level_dimensions[dimlevel][::-1]
-            options.update({
-                "original_shape": slideobj.dimensions[::-1],
-                "target_shape": tshape
-            })
-        elif imgarg.lower() == "auto":
-            imgarg = os.path.splitext(p.xml_file)[0] + ".svs"
-            import openslide
-            slideobj = openslide.open_slide(imgarg)
-            dimlevel = 0 if p.tile else -1
-            tshape = options["target_shape"] \
-                     or slideobj.level_dimensions[dimlevel][::-1]
-            options.update({
-                "original_shape": slideobj.dimensions[::-1],
-                "target_shape": tshape
-            })
-        else:
-            raise ValueError("Invalid string after --image flag. Expected "
-                             "either 'auto' for automatic inference from "
-                             "adjacent file, or the full path to the original "
-                             "image file.")
-
-    elif len(imgarg) == 2:
-        options.update({"original_shape": [int(d) for d in imgarg]})
+    # Determine resolution level
+    if p.resolution.lower() == "high":
+        dimlevel = 0
+    elif p.resolution.lower() == "low":
+        dimlevel = -1
     else:
-        raise ValueError("Invalid argument for original image size. Expected "
-                         "two integers for (vertical, horizontal) shape "
-                         "definition, or 'auto' for automatic inference from "
-                         "adjacent SVS file, or path to the original image "
-                         "file.")
+        try:
+            dimlevel = int(p.resolution)
+        except ValueError:
+            raise ValueError("Invalid input for --resolution: {}"
+                             .format(p.resolution))
+    options.update({"dimlevel": dimlevel})
 
+    # Infer original image size by opening SVS file (if --image is auto or path)
+    if os.path.isfile(p.image):
+        import openslide
+        slideobj = openslide.open_slide(p.image)
+        tshape = options["target_shape"]
+        options.update({
+            "original_shape": slideobj.level_dimensions[dimlevel][::-1],
+            "target_shape": tshape
+        })
+    elif p.image.lower() == "auto":
+        imgarg = os.path.splitext(p.xml_file)[0] + ".svs"
+        import openslide
+        slideobj = openslide.open_slide(imgarg)
+        tshape = options["target_shape"]
+        options.update({
+            "original_shape": slideobj.level_dimensions[dimlevel][::-1],
+            "target_shape": tshape
+        })
+    else:
+        raise ValueError("Invalid string after --image flag. Expected "
+                         "either 'auto' for automatic inference from "
+                         "adjacent file, or the full path to the original "
+                         "image file.")
+
+    # Set resolution level scale
+    dimscale_x, dimscale_y = np.divide(slideobj.level_dimensions[dimlevel],
+                                       slideobj.level_dimensions[0])
+    options.update({"dimscale_x": dimscale_x,
+                    "dimscale_y": dimscale_y})
+
+    # Raise error if the --tile option is specified without the --image option.
     if p.tile:
         if p.image is None:
             raise AttributeError("The --image option must be specified if the "
                                  "--tile option is used.")
         options.update({"tile": tuple(int(val) for val in p.tile)})
+
+    # Raise error if the --histo argument was specified without the
+    # --image argument.
+    if p.histo:
+        if p.image is None:
+            raise AttributeError("The --image option must be specified if the "
+                                 "--histo option is used.")
+
+    # Obtain path to the image file
+    if p.image.lower() != "auto":
+        if not os.path.isfile(p.image):
+            raise FileNotFoundError("Input image was not found at: {}"
+                                    .format(p.image))
+        else:
+            options.update({"image": p.image})
+    else:
+        imfile = p.xml_file[:p.xml_file.lower().index(".xml")] + ".svs"
+        options.update({"image": imfile})
 
     roi.process(p.xml_file, **options)
     if p.verbose > 0:
@@ -153,7 +186,7 @@ def init():
     # Define argument parser
     parser = argparse.ArgumentParser(
         prog="xml2mask",
-        description="Convert Aperio Image Analsys XML histology annotation "
+        description="Convert Aperio Image Analysis XML histology annotation "
                     "files to binary masks.")
     set_args(parser)
 
